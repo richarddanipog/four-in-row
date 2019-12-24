040d57a538b43b132e9bc249ebebb318e8fb14e6
richarddanipog <r.danipog@gmail.com>
2019-12-24 16:47:10 +0200

    add components

---
+++ src/components/board.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Board extends Component {
+
+    initBoard = (r,c) => {
+        
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Board;
