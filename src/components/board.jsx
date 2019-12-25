e6e0d84528e2e11e6e0a93ef71d06ab7cbc1a86a
richarddanipog <r.danipog@gmail.com>
2019-12-25 11:04:46 +0200

    work finally

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -6,11 +6,11 @@ class Board {
         for (let row = 0; row < rowInput; row++) {
             let matrixRow = []
             for (let col = 0; col < colInput; col++) {
-                matrixBoard.push(null)
+                this.matrixBoard.push(null)
             }
-            this.state.matrixBoard.push(matrixRow);
+            this.matrixBoard.push(matrixRow);
         }
-        return this.state.matrixBoard
+        return this.matrixBoard
     }
 
 
@@ -19,19 +19,16 @@ class Board {
         for (let row = board.length - 1; row >= 0; row--) {
             if (!(board[row][colorIndex])) {
                 board[row][colorIndex] = playerColor;
-            } else {
-                return alert('You can\'t play this move, try somthing else.');
-            }
+                return board[row][colorIndex]
+            } 
         }
-        return board[row][colorIndex]
-
     }
 
-    doWeHaveAWinner = () => {
-        return checkVertical(board, row, col) || checkHorizontal(board, row, col) || 
-               checkDiagonalRight(board, row, col) || checkDiagonalLeft(board, row, col)
+    doWeHaveAWinner = (board, row, col) => {
+        return this.checkVertical(board, row, col) || this.checkHorizontal(board, row, col) || 
+               this.checkDiagonalRight(board, row, col) || this.checkDiagonalLeft(board, row, col)
     }
-    checkVertical(board, row, col) {
+    checkVertical=(board, row, col)=> {
         // Check only if row is 3 or greater
         for (let r = 3; r < row; r++) {
             for (let c = 0; c < col; c++) {
@@ -46,7 +43,7 @@ class Board {
         } return false;
     }
 
-    checkHorizontal(board, row, col) {
+    checkHorizontal =(board, row, col) => {
         // Check only if column is 3 or less
         for (let r = 0; r < row; r++) {
             for (let c = 0; c < col; c++) {
@@ -61,7 +58,7 @@ class Board {
         } return false;
     }
 
-    checkDiagonalRight(board, row, col) {
+    checkDiagonalRight =(board, row, col) => {
         // Check only if row is 3 or greater AND column is 3 or less
         for (let r = 3; r < row; r++) {
             for (let c = 0; c < col; c++) {
@@ -76,7 +73,7 @@ class Board {
         } return false;
     }
 
-    checkDiagonalLeft(board, row, col) {
+    checkDiagonalLeft =(board, row, col) =>{
         // Check only if row is 3 or greater AND column is 3 or greater
         for (let r = 3; r < row; r++) {
             for (let c = 3; c < col; c++) {
