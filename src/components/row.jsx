35ee35e3f88f27b4ee761385a512904fb20554d6
richarddanipog <r.danipog@gmail.com>
2019-12-28 17:47:13 +0200

    fix css

--- src/components/row.jsx
+++ src/components/row.jsx
@@ -4,7 +4,7 @@ import  Cell  from './cell';
 
 const Row = ({row,playMove}) => {
     return (
-        <div className={'row'}>
+        <div className={'row m-0'}>
             {row.map((cell, i) => <Cell key={i} value={cell} columnIndex={i} playMove={playMove}/>)}
         </div>
     );
