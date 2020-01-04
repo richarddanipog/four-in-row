16882c35d1514f70bd46d5223ff1c70a6660bda3
richarddanipog <r.danipog@gmail.com>
2020-01-04 13:01:27 +0200

    add game mode best of

--- src/components/game-mode.jsx
+++ src/components/game-mode.jsx
@@ -7,7 +7,7 @@ class GameMode extends Component {
         console.log(this.props)
         return (
             <div className={'game-mode'}>
-                <div>
+                <div className={'mt-3'}>
                     <h1>Choose Game Mode</h1>
                 </div>
                 <form className={'choose-player radio-toolbar'}>
@@ -27,6 +27,16 @@ class GameMode extends Component {
 
                         <input type="radio" id="radio3" name="board" value="true" />
                         <label for="radio3">8 cols × 7 rows</label>
+                    </div>
+                    <div class="radio-toolbar">
+                        <input type="radio" id="game1" name="numOfGames" value="all" />
+                        <label for="game1">Single Game</label>
+
+                        <input type="radio" id="game2" name="numOfGames" value="false" />
+                        <label for="game2">Best of 3</label>
+
+                        <input type="radio" id="game3" name="numOfGames" value="true" />
+                        <label for="game3">Best of 5</label>
                     </div>
                 </form>
                 <Link to={'/game'} className={'start position-absolute'} >
